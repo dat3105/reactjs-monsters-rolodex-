a0a369c6aeef0e69c80ceefcd414bba728bcc6ca
TienDat <raiphantome@gmail.com>
2020-01-30 13:58:55 +0700

    Updates

---
+++ precache-manifest.9a2bfec5732e65ea6b5f334b11cba471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ef94582b44aab5d983487552fda225",
+    "url": "/dat3105/reactjs-monsters-rolodex-/index.html"
+  },
+  {
+    "revision": "4aaf7aa07209b117e80a",
+    "url": "/dat3105/reactjs-monsters-rolodex-/static/css/main.103c78c5.chunk.css"
+  },
+  {
+    "revision": "cc16885f54ea71240258",
+    "url": "/dat3105/reactjs-monsters-rolodex-/static/js/2.7a7557e0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dat3105/reactjs-monsters-rolodex-/static/js/2.7a7557e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4aaf7aa07209b117e80a",
+    "url": "/dat3105/reactjs-monsters-rolodex-/static/js/main.8672f173.chunk.js"
+  },
+  {
+    "revision": "b585610baf5ff1a06016",
+    "url": "/dat3105/reactjs-monsters-rolodex-/static/js/runtime-main.1fbfe0e4.js"
+  }
+]);
